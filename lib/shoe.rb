5dbc58ee874dfe4bce283b773ba876f93ab000ec
Kat Maldon <katharinamaldon@gmail.com>
2019-02-02 22:08:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,38 @@
-class Shoe 
-  attr_accessor :color, :size, :materiAL, :condition
-  
-  def intitialize(brand)
+class Shoe
+  def initialize(brand)
     @brand = brand
-  end 
-  
-  
-end 
+  end
+ 
+  def brand
+    @brand
+  end
+ 
+  def author=(author)
+    @author = author
+  end
+ 
+  def author
+    @author
+  end
+ 
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+ 
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
